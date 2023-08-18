4c35c1bb3c2920acb82482db3c69a86d1ceae529
eswar2005 <131540117+eswar2005@users.noreply.github.com>
2023-08-18 11:56:51 +0530

    import java.util.*; public class Es{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n=sc.nextInt();         for(int i=3;i<=n*3;i+=3){             System.out.print(i+" ");
            }
        }
    }

---
+++ First_N_multiples_of_3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Es{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=3;i<=n*3;i+=3){
+            System.out.print(i+" ");
+            
+        }
+    }
+}
