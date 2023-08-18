f211bc2d04f0e75d2491306339c79b293e91b6a8
eswar2005 <131540117+eswar2005@users.noreply.github.com>
2023-08-18 23:04:07 +0530

    import java.util.*; public class Es{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n,t;         n=sc.nextInt();         t=n*(n+ 1)/2;         System.out.println(t);     } }

---
+++ Sum_of_first_N_natural_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Es{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n,t;
+        n=sc.nextInt();
+        t=n*(n+ 1)/2;
+        System.out.println(t);
+    }
+}
