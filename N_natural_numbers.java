26e5e7b08215e98b8930eb8601d8f5c96aab0367
eswar2005 <131540117+eswar2005@users.noreply.github.com>
2023-08-18 12:04:14 +0530

    import java.util.*; public class Es{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         //int m=sc.nextInt();         int n=sc.nextInt();         int c=0;         for(int i=1;i<=n;i++){             System.out.print(i + " ");
            }
            
        }
    }

---
+++ N_natural_numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Es{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //int m=sc.nextInt();
+        int n=sc.nextInt();
+        int c=0;
+        for(int i=1;i<=n;i++){
+            System.out.print(i + " ");
+
+        }
+        
+    }
+}
