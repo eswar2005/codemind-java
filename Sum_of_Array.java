522e0043fa2d70f019fd5a6d6a0134f348fa147c
eswar2005 <131540117+eswar2005@users.noreply.github.com>
2023-08-19 11:46:01 +0530

    import java.util.*; public class Es{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int intArr[]=new int[n];         for(int i=0;i<n;i++){            intArr[i]=sc.nextInt();         }         int sum=0;         for(int v :intArr){             sum+=v;         }         System.out.println(sum);     } }

---
+++ Sum_of_Array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Es{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int intArr[]=new int[n];
+        for(int i=0;i<n;i++){
+           intArr[i]=sc.nextInt(); 
+        }
+        int sum=0;
+        for(int v :intArr){
+            sum+=v;
+        }
+        System.out.println(sum);
+    }
+}
