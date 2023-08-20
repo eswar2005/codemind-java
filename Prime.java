89b041ee029d83e37ed87531a296c80e4f4d8f6f
eswar2005 <131540117+eswar2005@users.noreply.github.com>
2023-08-20 12:19:21 +0530

    import java.util.Scanner; public class Es{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a=sc.nextInt();         //int b=sc.nextInt();         int c=0;         for(int i=1;i<=a;i++){             if(a%i==0)             c++;         }         if(c>2){              System.out.print("Not Prime");         }         else{              System.out.print("Prime");         }
        }
    }

--- Prime.java
+++ Prime.java
@@ -1,19 +1,20 @@
-import java.util.*;
+import java.util.Scanner;
 public class Es{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
+        int a=sc.nextInt();
+        //int b=sc.nextInt();
         int c=0;
-       for(int i=1;i<=n;i++){
-           if(n%i==0){
-               c++;
-           }
-       }
-       if(c==2){
-           System.out.println("Prime");
-       }
-       else{
-           System.out.println("Not Prime");
-       }
+        for(int i=1;i<=a;i++){
+            if(a%i==0)
+            c++;
+        }
+        if(c>2){
+             System.out.print("Not Prime");
+        }
+        else{
+             System.out.print("Prime");
+        }
+      
     }
 }
