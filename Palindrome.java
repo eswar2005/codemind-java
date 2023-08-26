3c10aedd59f8fe508f398095da0e58b0011e84a4
eswar2005 <131540117+eswar2005@users.noreply.github.com>
2023-08-26 10:36:18 +0530

    import java.util.*; public class Es{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n,rev=0,r;         n=sc.nextInt();         int q=n;         while(n!=0){             r=n%10;             rev=rev*10+r;             n/=10;         }         if(q==rev){             System.out.println("True");         }         else{             System.out.println("False");         }
        }
    }

--- Palindrome.java
+++ Palindrome.java
@@ -2,19 +2,20 @@ import java.util.*;
 public class Es{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        int rev=0,o=n,r;
-        //n=sc.nextInt();
+        int n,rev=0,r;
+        n=sc.nextInt();
+        int q=n;
         while(n!=0){
             r=n%10;
             rev=rev*10+r;
             n/=10;
         }
-       if(rev==o){
-           System.out.println("Palindrome");
-       }
-       else{
-           System.out.println("Not Palindrome");
-       }
+        if(q==rev){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        } 
+        
     }
 }
