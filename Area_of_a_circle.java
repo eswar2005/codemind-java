31c964f9f3100ef4fcb4b84c67da53042029e048
eswar2005 <131540117+eswar2005@users.noreply.github.com>
2023-09-01 11:28:55 +0530

    import java.util.*; public class Area{     public static void main(String[] eswar){         Scanner sc = new Scanner(System.in);         float n = sc.nextFloat();         double area = 3.14*n*n;         System.out.printf("%.2f",area);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Area{
+    public static void main(String[] eswar){
+        Scanner sc = new Scanner(System.in);
+        float n = sc.nextFloat();
+        double area = 3.14*n*n;
+        System.out.printf("%.2f",area);
+    }
+}
