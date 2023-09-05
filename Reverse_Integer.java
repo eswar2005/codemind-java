bab391c5838db678ef9682a86093e7622f5c493b
eswar2005 <131540117+eswar2005@users.noreply.github.com>
2023-09-05 16:04:39 +0530

    import java.util.*; public class Es{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int r,rev=0;         while(n!=0){             r=n%10;             rev=rev*10+r;             n/=10;         }         System.out.println(rev);     } }

--- Reverse_Integer.java
+++ Reverse_Integer.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class Es{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n,rev=0,r;
-        n=sc.nextInt();
+        int n = sc.nextInt();
+        int r,rev=0;
         while(n!=0){
             r=n%10;
             rev=rev*10+r;
