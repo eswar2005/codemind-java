413ab440aa09db220a96f6b2f75416f1e28edd33
eswar2005 <131540117+eswar2005@users.noreply.github.com>
2023-09-07 20:51:09 +0530

    import java.util.*; public class CountElements{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int k = sc.nextInt();         int[] arr = new int[n];         for(int i=0;i<n;i++){             arr[i] = sc.nextInt();         }         int c=0;         for(int i=0;i<n;i++){             if(arr[i]%k!=0){                 c++;             }         }         System.out.println(c);     } }

--- Count_elements_not_divisible_by_K.java
+++ Count_elements_not_divisible_by_K.java
@@ -4,16 +4,16 @@ public class CountElements{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        int arr[] = new int[n];
+        int[] arr = new int[n];
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-        int count=0;
-        for(int v : arr){
-            if(v % k !=0){
-                count+=1;
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]%k!=0){
+                c++;
             }
         }
-        System.out.println(count);
+        System.out.println(c);
     }
 }
